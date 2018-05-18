9efb495abca31f0e7e4ef42f402b59e2361162c2
孙文权 <604164227@qq.com>
2018-05-18 15:50:51 +0800

    add

---
+++ DesignPatterns/Builder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public abstract class Person
+    {
+        public abstract void BuildHead();
+        public abstract void BuildBody();
+        public abstract void BuildLag();
+    }
+    public class TinPerson : Person
+    {
+        public override void BuildHead()
+        {
+            Console.WriteLine("TinHead");
+        }
+        public override void BuildBody()
+        {
+            Console.WriteLine("TinBody");
+        }
+        public override void BuildLag()
+        {
+            Console.WriteLine("TinLag");
+        }
+    }
+    public class FatPerson : Person
+    {
+        public override void BuildHead()
+        {
+            throw new NotImplementedException();
+        }
+        public override void BuildBody()
+        {
+            throw new NotImplementedException();
+        }
+        public override void BuildLag()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Builder
+    {
+        public Person Person { get; }
+
+        public Builder(Person person)
+        {
+            this.Person = person;
+        }
+        public void Build(Person person)
+        {
+            person.BuildHead();
+            Person.BuildBody();
+            person.BuildLag();
+        }
+
+    }
+}
---
+++ DesignPatterns/Factory.cs
@@ -0,0 +1,46 @@
+﻿namespace DesignPatterns
+{
+    abstract class Operat
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+        public virtual double GetResult()
+        {
+            return 0;
+        }
+    }
+    class Add : Operat
+    {
+        public override double GetResult()
+        {
+            return NumberA + NumberB;
+        }
+    }
+    class Sub : Operat
+    {
+        public override double GetResult()
+        {
+            return NumberA - NumberB;
+        }
+    }
+
+    /// <summary>
+    /// 工厂类
+    /// </summary>
+    class Factory
+    {
+
+        public static Operat GetOperat(string flag)
+        {
+            switch (flag)
+            {
+                case "1":
+                    return new Add();
+                case "2":
+                    return new Sub();
+                default:
+                    return null;
+            }
+        }
+    }
+}
